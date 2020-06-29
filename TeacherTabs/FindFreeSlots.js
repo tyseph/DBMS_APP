216c5b12eebf3f7c82192fbfde2feddf80ec1fd0
tyseph <sg748@snu.edu.in>
2020-06-29 18:48:35 +0530

    29 june 7 pm update

--- TeacherTabs/FindFreeSlots.js
+++ TeacherTabs/FindFreeSlots.js
@@ -43,9 +43,13 @@ import {
           <Text style={styles.TextStyle}>FIND FREE SLOTS</Text>
         </LinearGradient>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.SubmitButtonStyle} activeOpacity = { 0.5 } onPress={()=>{this.props.navigation.navigate('Login')}} >
-        <LinearGradient colors={['#36D6BD','#007E7B']} start={{ x:0, y:1}} style={ styles.Linear_G }>	
-          <Text style={styles.TextStyle}>LOGOUT</Text>
+        <View style={styles.timing}>
+          <Text style={styles.textOutput}>Timings :</Text>
+        <TextInput textContentType='addressCity' placeholderTextColor="#fff" style = { styles.textOutput_Style } />
+        </View>
+        <TouchableHighlight style={styles.LogoutButtonStyle} activeOpacity = { 0.5 } onPress={()=>{this.props.navigation.navigate('Login')}} >
+        <LinearGradient colors={['#A23434','#7E1600']} start={{ x:0, y:2}} style={ styles.Linear_G }>	
+          <Text style={styles.LogoutTextStyle}>LOGOUT</Text>
         </LinearGradient>
         </TouchableHighlight>
         </View>
@@ -86,10 +90,33 @@ import {
         marginLeft: 10,
         marginRight: 10
       },	
-      
+      textOutput_Style:
+      {
+        borderWidth: 0,
+        borderBottomColor: '#fff',
+        borderBottomWidth: 2,
+        fontSize: 25,
+        color: '#fff',
+        width: 270,
+        fontFamily: 'Roboto-Thin', 
+        padding: 0
+      },	
+      textOutput:
+      {
+        fontSize: 25,
+        color: '#fff',
+        fontFamily: 'Roboto-Thin', 
+        paddingBottom: 0,
+      },	
+      timing:
+      {
+        marginTop: 50,
+        flex: 0,
+        flexDirection: 'row'
+      },      
       SubmitButtonStyle: {
         
-        marginTop: 20,
+        marginTop: 50,
         padding: 0,
         backgroundColor: '#36D6BD',
         borderRadius: 10,
@@ -115,7 +142,24 @@ import {
       Button_align:
       {
         alignItems: "center",
-        justifyContent: 'center'
-      }
+        justifyContent: 'space-evenly'
+      },
+      LogoutButtonStyle: {
+
+        marginTop: 500,
+        padding: 0,
+        backgroundColor: '#36D6BD',
+        borderRadius: 10,
+        borderWidth: 0,
+        width: 250
+      },
+      LogoutTextStyle:{
+        color:'#fff',
+        textAlign:'center',
+        fontSize: 30,
+        fontWeight: '100',
+        fontFamily: 'Roboto-Light'
+        
+      },
     });
   
